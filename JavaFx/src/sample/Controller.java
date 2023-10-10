e7e4ca44591f0a95a3ee203a8bc87de45d2e9546
Khanh Duong Quoc <quockhanh.hsgs@gmail.com>
2023-10-10 14:32:17 +0700

    abc

---
+++ JavaFx/src/sample/Controller.java
@@ -0,0 +1,11 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+public class Controller {
+}
